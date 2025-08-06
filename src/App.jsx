dd19775e4d4c1241097702b530ecfbba1119519c
elgun030 <elgunng@code.edu.az>
2025-08-06 13:45:53 +0400

    add repooo tokennn

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home/page";
 import Header from "./Components/Layout/Header";
@@ -9,24 +9,64 @@ import SuggestionsPage from "./Pages/SuggestionsPage/page";
 import ContactPage from "./Pages/ContactPage/page";
 import AboutPage from "./Pages/AboutPage/page";
 import SolutionsPage from "./Pages/SolutionsPage/page";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-function App() {
+const AppContent = () => {
+  const [token, setToken] = useState(localStorage.getItem("token") || null);
+
+  useEffect(() => {
+    if (token) {
+      return;
+    }
+
+    axios
+      .post(`https://api.selnaz.com:9098/selnaz/user`, {
+        // əgər body göndərmək lazımdırsa, bura əlavə et
+      })
+      .then((res) => {
+        if (res.data) {
+          localStorage.setItem("token", res.data);
+          setToken(res.data);
+          axios.defaults.headers.common["Authorization"] = `Bearer ${res.data}`;
+        } else {
+          console.warn("Token cavabda tapılmadı!");
+        }
+      })
+      .catch((err) => {
+        console.error("Token alına bilmədi:", err);
+      });
+  }, [token]);
+
+  if (!token) {
+    return <div>Yüklənir...</div>;
+  }
+
   return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />}  className/>
-        <Route path="/SuggestionsPage" element={<SuggestionsPage />} />
-        <Route path="/ContactPage" element={<ContactPage />} />
-        <Route path="/AboutPage" element={<AboutPage />} />
-        <Route path="/SolutionsPage" element={<SolutionsPage />} />
-      </Routes>
-      <div className="relative">
-        <Footer />
-        <FooterElem />
+    <>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/SuggestionsPage" element={<SuggestionsPage />} />
+          <Route path="/ContactPage" element={<ContactPage />} />
+          <Route path="/AboutPage" element={<AboutPage />} />
+          <Route path="/SolutionsPage" element={<SolutionsPage />} />
+        </Routes>
+        <div className="relative">
+          <Footer />
+          <FooterElem />
+        </div>
       </div>
-    </div>
+    </>
   );
-}
+};
 
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  );
+};
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,7 @@
+window.global = window;
+
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import { ThemeProvider } from "./Components/Layout/ThemeProvider.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <ThemeProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>
-);
+createRoot(document.getElementById("root")).render(<App />);
