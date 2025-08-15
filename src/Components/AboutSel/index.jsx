e95d7de9b2729dad86921e76d5777fed9f28c702
elgun030 <elgunng@code.edu.az>
2025-08-15 15:01:14 +0400

    added fixeddd

--- src/Components/AboutSel/index.jsx
+++ src/Components/AboutSel/index.jsx
@@ -4,8 +4,8 @@ const AboutSel = () => {
   return (
     <div className="mt-[130px] md:mt-[363px] px-4 text-[#3D246A] md:flex md:flex-col md:items-center md:text-center md:justify-center md:w-full openSans">
       {/* Text box */}
-      <div className="w-full  md:w-[606px] border border-white/30 shadow-[0_4px_30px_rgba(0,0,0,0.1)] bg-gradient-to-r from-white/10 via-white/5 to-white/10 backdrop-blur-[10px] backdrop-saturate-150 rounded-[20px]">
-        <div className="flex flex-col items-center text-center">
+      <div className="w-full   md:py-[24px]   md:w-[606px] border border-white/30 shadow-[0_4px_30px_rgba(0,0,0,0.1)] bg-gradient-to-r from-white/10 via-white/5 to-white/10 backdrop-blur-[10px] backdrop-saturate-150 rounded-[20px]">
+        <div className="flex  flex-col items-center text-center">
           <div className="py-[20px] md:py-0 md:mb-[18px]">
             <h2 className="font-bold md:text-[28px] dark:text-[#E1DCE6] text-[22px] text-[#3D246A]">
               Niyə məhz Selnaz?
@@ -39,7 +39,7 @@ const AboutSel = () => {
       />
       {/* Desktop */}
       <img
-        className="hidden bottom-[100px]  md:block mt-[-598px] mx-auto"
+        className="hidden bottom-[100px]  md:block mt-[-648px] mx-auto"
         src="https://res.cloudinary.com/duy7rcf4m/image/upload/v1754899480/aboutHuge_azzsvd.svg"
         alt="about"
       />
